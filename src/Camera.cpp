34d9f8f20f4fbc4fbbba4f18299ea5196f961364
Peaches_MLG <PeachesMLG100@gmail.com>
2023-11-13 23:23:01 +0000

    :thinking:

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -8,10 +8,10 @@ void Camera::applyView(GLuint shaderId, Player player) {
     auto view = glm::mat4(1.0f);
 
     // Apply translation
-//    view = glm::translate(view, glm::vec3(player.x, player.y, 0));
+    view = glm::translate(view, glm::vec3(-player.x, -player.y, 0));
 
     // Apply zoom (scaling)
-    view = glm::scale(view, glm::vec3(zoom, zoom, 1.0f));
+//    view = glm::scale(view, glm::vec3(zoom, zoom, 1.0f));
 
     int viewLog = glGetUniformLocation(shaderId, "view");
     glUniformMatrix4fv(viewLog, 1, GL_FALSE, glm::value_ptr(view));
